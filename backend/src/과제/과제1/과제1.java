9f01bbb356274d0a6f41269842cddd6546ae94b8
504 <504@DESKTOP-T7B8DGK>
2023-07-13 17:22:22 +0900

    0713_과제1commit

---
+++ backend/src/과제/과제1/과제1.java
@@ -0,0 +1,45 @@
+package 과제.과제1;
+
+public class 과제1 {
+
+	public static void main(String[] args) {
+		
+		// 문제1 : 4
+		// 문제2 : 2, 3, 4, 5, 6, 7
+		/* 문제3 : 
+			정수타입 : 
+				1byte - byte
+				2byte - short, char
+				4byte - int
+				8byte - long
+			실수타입 : 
+				4byte - float
+				8byte - double
+			논리타입 : 
+				1byte - boolean
+		*/
+		/* 문제4 : 
+			타입 : int, double
+			변수명 : age, price
+			리터럴 : 10, 3.14
+		*/
+		// 문제5 : 3
+		// 문제6 : 4
+		// 문제7 : 6
+		// 문제8 : 3
+		// 문제9 : 1
+		// 문제10 : 2
+		/* 문제11 : 
+			13번째, 16번째 줄
+		*/
+	}
+
+}
+
+/* 
+	1. (2인1조) 연산자 암기 확인 2인 1조
+	
+	2. (1인) p.73 ~ p.76 풀이 => 카톡방 과제1 git 주소 
+		
+
+*/
