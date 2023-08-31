6bc224cea7d8861d3c89b53382e81ef5f1d0eaf6
504 <504@DESKTOP-E6CD24T>
2023-08-31 14:08:10 +0900

    0831 commit

--- jspweb/src/main/java/model/dao/MemberDao.java
+++ jspweb/src/main/java/model/dao/MemberDao.java
@@ -1,73 +1,80 @@
 package model.dao;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 import model.dto.MemberDto;
-import model.dto.VisitDto;
 
-public class MemberDao extends Dao{
-
-	private static MemberDao visitDao = new MemberDao();
-	public static MemberDao getInstance() { return visitDao; }  
+public class MemberDao extends Dao {
+	// 0. 싱글톤
+	private static MemberDao memberDao = new MemberDao();
 	private MemberDao() {}
+	public static MemberDao getInstance() { return memberDao; }
 	
 	// 1. 회원가입
-	public boolean signup(MemberDto dto) {
-	
-		String sql="insert into member(mid,mpwd,memail,mimg) values(?,?,?,?)";
-		
+	public boolean signup( MemberDto dto ) {
 		try {
+			String sql = "insert into member( mid , mpwd , memail , mimg ) values( ? , ? , ? , ? )";
 			ps = conn.prepareStatement(sql);
-			ps.setString(1, dto.getMid());
-			ps.setString(2, dto.getMpwd());
-			ps.setString(3, dto.getMemail());
-			ps.setString(4, dto.getMimg());
+			ps.setString( 1 , dto.getMid() );
+			ps.setString( 2 , dto.getMpwd() );
+			ps.setString( 3 , dto.getMemail() );
+			ps.setString( 4 , dto.getMimg() );
 			int row = ps.executeUpdate();
-			if(row == 1) return true;
-		}catch(Exception e) {
-			System.out.println("signup 오류 : " + e);
-		}
-		
+			if( row == 1 )return true;
+		}catch (Exception e) {System.out.println(e);}
 		return false;
 	}
-	// 2. 로그인
-	public boolean login(String mid, String mpwd) {
+	// 2. 로그인 
+	public boolean login( String mid , String mpwd ) {
 		try {
-			String sql = "select * from member where mid = ? and mpwd = ?";
-			
+			String sql ="select * from member where mid =  ? and mpwd = ? ";
 			ps = conn.prepareStatement(sql);
-			ps.setString(1, mid);
-			ps.setString(2, mpwd);
+			ps.setString( 1 , mid ); ps.setString( 2 , mpwd );
 			rs = ps.executeQuery();
-			if(rs.next()) return true;
-		}catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("login DB오류 : " + e);
-		}
+			if( rs.next() ) return true;
+		}catch (Exception e) { System.out.println(e);}
 		return false;
 	}
 	// 3. 아이디찾기
 	
-	// 4. 비밀번호찾기
+	// 4. 비밀번호찾기 
 	
-	// 5. 내정보 호출
-	
-	// 6. 아이디/이메일 중복 검사 
-	public boolean findIdOrEmail(String type, String data ) {
-		String sql = "select * from member where " + type + " = ?";
-		
+	// 5. 내정보 호출 
+	public MemberDto info( String mid ) {
 		try {
-			
+			String sql ="select mno , mid , memail , mimg from member where mid = ?";
 			ps = conn.prepareStatement(sql);
-			ps.setString(1, data);
+			ps.setString( 1 , mid );
 			rs = ps.executeQuery();
-			if(rs.next()) return true;
-			
-		}catch(Exception e) {
-			System.out.println("findIdOrEmail 에러" + e);
-		}
-		
+			if( rs.next() ) {
+				MemberDto memberDto = new MemberDto( 
+						LocalDateTime.now().toString() , 
+						// LocalDateTime.now().toString() : 현재 날짜/시간 문자열 반환 함수 
+						rs.getInt(1), rs.getString(2),
+						rs.getString(3), rs.getString(4));
+				return memberDto;
+			}
+		}catch (Exception e) { System.out.println(e ); }
+		return null;
+	}
+	
+	// 6. 아이디/이메일 중복검사 [ 인수 : 검사할아이디 / 반환 : true(중복있어) , false(중복없어) 
+		// - type(mid,memail) : 필드명 / data(입력받은mid,memail) : 필드에서 찾을 값 
+	public boolean findIdOrEmail( String type , String data ) {
+		try {
+			String sql = "select * from member where "+type+" = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setString( 1 , data );
+			rs = ps.executeQuery();
+			// [ while : 결과 레코드 여러개 검사 vs if : 결과 레코드 한개 검사 ]
+			if( rs.next() ) return true;
+		}catch (Exception e) {System.out.println(e);}
 		return false;
 	}
+	// 7. 회원수정
+	
+	// 8. 회원탈퇴
+	
+	
+	
 }
-
