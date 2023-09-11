3e20b9b8bb2e6e5042a3b70a6b6d8578fc601dfb
504 <504@DESKTOP-G2B2I5O>
2023-09-11 13:47:05 +0900

    소켓 commit

--- jspweb/src/main/webapp/js/chatting/chatting.js
+++ jspweb/src/main/webapp/js/chatting/chatting.js
@@ -1,28 +1,181 @@
-console.log('chatting.js')
 
-// 1. 클라이언트 소켓 만들기
+// * ajax(기본값 비동기통신) 가 응답의 결과를 가지고 오기전에 아래코드 실행. [ loginState 변경되기전에 실행되므로 문제발생 ]
+// - 해결방안 ajax 동기화 통신 하자.. 
+// 1. 비 로그인시 입장 불가능.. 
+if( loginState == false ){ alert('회원전용 페이지입니다.'); location.href="/jspweb/member/login.jsp"; }
+
+// 2. JS 클라이언트[유저] 소켓 만들기 
+console.log( '채팅방에 입장한 아이디 : ' + loginMid );
+let clientSocket = new WebSocket(`ws://localhost:80/jspweb/serversokcet/${loginMid}`);
+	// - 클라이언트소켓이 생성되었을때 자동으로 서버소켓에 접속 ----> 서버소켓의 @OnOpen 으로 이동
+	// - 서버소켓URL에 매개변수 전달하기 [- 주로 식별자 전달 ] 서버소켓URL/데이터1/데이터2/데이터3
 	
-	// 1. JS웹소켓 객체 [WebSocket클래스]
-		// new WebSocket('서버소켓 URL')
-	let 클라이언트소켓 = new WebSocket('ws://localhost:80/jspweb/ChattingSocket'); console.log(클라이언트소켓);
 	
-	클라이언트소켓.onmessage = (e) => onmsg(e);
-	클라이언트소켓.onerror = (e) => {console.log(e)
-	console.log(' dfsdf')};
+	
+// ------------------------------------------------------------------------------ // 
+/*
+	JS[ HTML파일 종속된 파일 - HTML 안에서 실행되는 구조 ] - 추후 node.js 라는 프레임워크 는 HTML로부터 벗어남.
+	
+	* 
+		클라이언트 : 사용 데이터 요청 하는 PC
+		서버 : 데이터 제공하는 PC
+		
+	* URL[주소] 상의 매개변수 전달하는 방법
+		1. 쿼리스트링 방식 : URL?매개변수명1=데이터1&매개변수명2=데이터2&매개변수명3=데이터3
+		2. 경로매개변수 방식 : URL/데이터1/데이터2/데이터3
+		
+	* 소켓이란. 
+		- 통신의 종착점[데이터가 전달되는 위치 -받는사람 -보낸사람]
+		- 예시 
+		[클라이언트소켓]							[서버소켓]
+		JS=강호동집									우체국		
+			안산시 상록구[소켓]	----------------->					
+			
+		JS=신동엽집
+			수원시 영통구[소켓]	<------------------
+		
+	* WebSocket 클라이언트 웹 소켓 라이브러리 
+		- 소켓 관련된 함수들을 제공하는 클래스 
+		1. 웹소켓 객체 생성 
+			new WebSocket("ws://IP주소:PORT번호/프로젝트명/서버소켓경로");
+			
+	* 동기화 vs 비동기화 
+	
+	
+	동기화[ 요청1 결과에 따른 다음 요청이 필요할때 ]		비동기화[ 여러 기능을 별도로 처리 ]
+	
+	Client				Server					Client				Server
+	
+		--------요청1------->							--------요청1------->
+	대기상태o				요청1처리중					대기상태x					요청1처리중[5분]
+		<-------응답1--------							--------요청2------->
+																		요청2처리중	[1분]
+		--------요청2------->							<-------응답2--------
+						요청2처리중						
+		<-------응답2--------							<-------응답1--------
+		
+		
+	
+*/
 
 
-function msgsend() {
-	// 1. input 입력된 값 가져오기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// 1. 클라이언트 소켓 만들기 
+	// 1. JS웹소켓 객체 [ WebSocket클래스 ] / 객체가 생성되면 서버소켓에 자동으로 접속됨.
+		// new WebSocket('ws://ip주소:포트번호/프로젝트명/서버소켓URL');  [ 객체생성 =====> 서버소켓 @OnOpen ]
+	let 클라이언트소켓 = new WebSocket('ws://localhost:80/jspweb/ChattingSocket'); console.log( 클라이언트소켓 );
+		// 2. opopen , onclose , send , onmessage 등등 메소드 제공 
+	
+		// 내가 만든 함수를 클라이언소켓 속성에 대입
+	클라이언트소켓.onmessage = ( event ) => onmsg(event);
+	
+// 2. 연결된 서버소켓에게 메시지 보내기 
+function msgsend(){
+	// 1. input 입력된 값 가져오기 
 	let msg = document.querySelector('.inputcontent').value;
 	// 2. 클라이언트소켓 .send();
-	클라이언트소켓.send(msg); // input에서 입력받은 데이터를 보내기 [ 클라이언트.send() ===> 서버소켓 @OnMessage]
-}
+	클라이언트소켓.send( msg ); // input에서 입력받은 데이터를 보내기  [ 클라이언트소켓.send() =====> 서버소켓 @OnMessage ]
+	
+	// 3. 
+	document.querySelector('.inputcontent').value = '';
+}// f end 
 
-function onmsg(event) {
-	console.log(event);
+// 3. 연결된 서버소켓으로 부터 메시지를 받았을떄.
+function onmsg(event){ 
+	console.log(event) ;
 	console.log(event.data);
-	console.log('받음');
 	
 	document.querySelector('.contentbox')
-		.innerHTML += `<div>${event.data}</div>`
+				.innerHTML += `<div> ${event.data}</div>`
+	
 }
+*/
+
+/*
+	1. AJAX [ 단방향 ]
+		발신자(보내는사람) 	: AJAX		
+		수신자(받는사람) 	: 서블릿		
+	
+		JS					JAVA[서블릿]
+							
+		AJAX	
+				rest		get
+							post	
+							put
+							delete
+	2. (웹)소켓 [ 양방향 ]
+		소켓 : 통신의 종착점[도착위치]
+		- 전체조건 : 서버가 클라이언트소켓의 정보를 가지고 있어야한다.
+		
+		JS					JAVA
+		[클라이언트소켓]		[서버소켓]
+		websocket
+		1. 서버소켓 접속		2. 서버소켓 24시간 상시 클라이언트 소켓 명단 저장
+		3. 데이터 전송			4. 데이터 받는다.
+*/
+/*
+
+				클라언트[사용자]						서버[서비스회사]
+			
+		강호동카카오톡		
+						'안녕'
+				-------------------------------->
+				1. 강호동 메시지(안녕) 보낸다.
+				
+				<--------------------------------
+				3. 서버가 '안녕' 메시지 보낸다.
+				
+		유재석카카오톡 
+													카카오서버[제주도]
+													- 전제조건 : 클라이언트의 정보들 보관
+													접속된명단 : [ 강호동,유재석,신동엽,하하 ]
+													2. '안녕' 메시지 받는다.
+				<--------------------------------		
+				3. 서버가 '안녕' 메시지 보낸다.			
+		신동엽카카오톡 
+		
+				<--------------------------------
+				3. 서버가 '안녕' 메시지 보낸다.
+		하하카카오톡 
+				<--------------------------------
+				3. 서버가 '안녕' 메시지 보낸다.
+		
+*/
